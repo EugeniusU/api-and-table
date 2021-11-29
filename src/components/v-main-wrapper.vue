c67e474f4f2dbf98506c55ff7309633f57d47298
EugeniusU <59093008+EugeniusU@users.noreply.github.com>
2021-11-30 01:12:19 +0300

    Add files via upload

---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class='v-main-wrapper'>
+        <v-search-field/>
+        <v-table/>
+    </div>    
+</template>
+
+<script>
+import vTable from './v-table'
+import vSearchField from './v-search-field'
+
+export default({
+    name: 'v-main-wrapper',
+    components: {
+        vTable,
+        vSearchField
+    }
+})
+</script>
+
+<style>
+.v-main-wrapper {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    margin-top: 30px;
+}
+</style>
---
+++ src/components/v-search-field.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class='v-search-field'>
+        <i id='warning_search_length' class='material-icons'
+            v-if='query && query.length < 3'
+        >warning_amber</i>
+        <input type='text' v-model='query' placeholder='Запрос' @input='search'>
+        <button class='search_btn' @click='search'>
+            <i class='material-icons'>search</i>
+        </button>
+    </div>    
+</template>
+
+<script>
+import {mapActions} from 'vuex'
+
+export default({
+    name: 'v-search-field',
+    data() {
+        return {
+            query: ''
+        }
+    },
+    components: {},
+    methods: {
+        ...mapActions([
+            'GET_FILTRED_WITH_QUERY',
+            'GET_DATA_FROM_API'
+        ]),
+        search() {
+            let query = this.query.trim();
+
+            if (query && query.length >= 3) {
+                this.GET_FILTRED_WITH_QUERY(query)
+            } else if (!query.length || !query) {
+                console.log('this');
+                this.GET_DATA_FROM_API().then(console.log)
+            }
+
+            console.log(query, query.length);
+        }
+    },
+    computed: {}
+})
+</script>
+
+<style>
+.v-search-field {
+    display: flex;
+    align-self: flex-end;
+    margin-right: 100px;
+}
+
+.search_btn {
+    border: none;
+    background: transparent;
+}
+
+#warning_search_length {
+    color: orange;
+    width: 30px;
+    align-self: center;
+}
+</style>
