3388c44fb1c22f5fa0679cac6eb991b39d65cfa0
EugeniusU <59093008+EugeniusU@users.noreply.github.com>
2021-11-30 17:05:07 +0300

    Update v-main-wrapper.vue

--- src/components/v-main-wrapper.vue
+++ src/components/v-main-wrapper.vue
@@ -2,18 +2,28 @@
     <div class='v-main-wrapper'>
         <v-search-field/>
         <v-table/>
+        <v-loading-info v-if='!DATA.length'></v-loading-info>
     </div>    
 </template>
 
 <script>
 import vTable from './v-table'
 import vSearchField from './v-search-field'
+import vLoadingInfo from './v-loading-info'
+
+import {mapGetters} from 'vuex'
 
 export default({
     name: 'v-main-wrapper',
     components: {
         vTable,
-        vSearchField
+        vSearchField,
+        vLoadingInfo
+    },
+    computed: {
+        ...mapGetters([
+            'DATA'
+        ])
     }
 })
 </script>
