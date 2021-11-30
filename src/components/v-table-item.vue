771f9cd2889b96c09a887cef1264db3b032561c6
EugeniusU <59093008+EugeniusU@users.noreply.github.com>
2021-11-30 22:53:22 +0300

    Update v-table-item.vue

--- src/components/v-table-item.vue
+++ src/components/v-table-item.vue
@@ -2,22 +2,32 @@
     <div class='v-table-item'>
 
         <div class='info_wrap'>           
-            <div class='title'>
+            <div class='title node'>
                 <button class='list_btn' @click='showContact'>
                     <i class='material-icons' v-if='!isShowContact'>add</i>
                     <i class='material-icons' v-if='isShowContact'>remove</i>
                 </button>
                 <span>
                     {{tableRow.title}}
-                </span>    
+                </span>
+                <div class='tags'
+                    v-for='(tag, index) in tableRow.tags'
+                    :key='index'
+                >
+                   <div>{{tag}}</div>
+                </div>
             </div>
-            <div class='info__status'>{{tableRow.statusName}}</div>
-            <div class='info__user'>
+            <div class='info__status node'>
+                <span class='colorize_status' :style="{backgroundColor: tableRow.statusColor}">
+                {{tableRow.statusName}}
+                </span>
+            </div>
+            <div class='info__user node'>
                 <i class='material-icons'>account_circle</i>
                 <span>{{tableRow.user}}</span>
             </div>
-            <div class='info__created_time'>{{new Date(tableRow.createdAt * 1000).toLocaleString()}}</div>
-            <div class='info__price'>{{tableRow.price}}</div>
+            <div class='info__created_time node'>{{new Date(tableRow.createdAt * 1000).toLocaleString()}}</div>
+            <div class='info__price node'>{{tableRow.price}}</div>
         </div>
 
         <div class='contact_wrap' v-if='isShowContact'>
@@ -61,6 +71,11 @@ export default({
     flex-direction: column;
     min-height: 30px;
     margin-top: 15px;
+    border-bottom: 1px solid grey;
+}
+
+.v-table-item:hover {
+    background: lightgrey;
 }
 
 .info_wrap, .contact_wrap {
@@ -76,8 +91,24 @@ export default({
     justify-content: space-around;
 }
 
-.info_wrap > div, .contact > div {
+.node {
     flex-basis: 20%;
+}
+
+.node:first-child {
+    flex-basis: 30%;
+}
+
+.node:last-child {
+    flex-basis: 10%;
+}
+
+.node:nth-child(4n) {
+    flex-basis: 10%;
+}
+
+.node:nth-child(3n) {
+    flex-basis: 10%;
 }
 
 .contact > div {
@@ -118,6 +149,26 @@ export default({
 
 .v-table i {
     margin-right: 10px;
+}
+
+.colorize_status {
+    padding: 5px;
+}
+
+.list_btn i {
+    margin-right: 0;
+}
+
+.tags {
+    align-self: center;
+    display: flex;   
+    margin-left: 10px;
+}
+
+.tags div {
+    margin-left: 5px;
+    background-color: lightgreen;
+    padding: 2px 4px;
 }
 
 </style>
