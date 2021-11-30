5277ac20e2339de10be4b26be40cf7f39ea54a6e
EugeniusU <59093008+EugeniusU@users.noreply.github.com>
2021-11-30 17:24:08 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+      <v-main-wrapper/>
+  </div>
+</template>
+
+<script>
+import VMainWrapper from './components/v-main-wrapper.vue'
+
+export default {
+  name: 'App',
+  components: {
+    VMainWrapper
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+/*  margin-top: 60px; */
+  
+
+  width: 100%;
+  height: 100%;
+  position: absolute;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
